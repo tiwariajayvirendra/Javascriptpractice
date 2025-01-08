f878d376efd4421363fe6506496e2751a3b5a2be
Tiwari Ajay Virendra <ajaytripathi821@gmail.com>
2025-01-08 13:17:52 +0530

    Your commit message

---
+++ objct.js
@@ -0,0 +1,197 @@
+// This is an example of Object (a collection of DataTypes like ,
+// [<1 String = "under these double inverted comma" ,
+//   2 Number = 123456,
+//   3 Boolean = true and false,
+//   4 Undefined  let a;,
+//   5 Null let a= ;,
+//   6 BigInt 4.565,
+//   7 Symbol * 
+// > )
+////////////////////////////////////////////////////////////////////////////////
+// const profile = {
+//     username : "Ajay Tiwari",
+//     folowers : 1999999,
+//     following: 200,
+//     isfollow :true,
+//     posts    : 46,
+//     info     : "male ",
+//     age      : 35,
+// };
+// console.log(typeof profile["age"]);
+////////////////////////////////////////////////////////////////////////////////
+// Arthematic operators [ + , _  , * , / ],
+// let a = 9;
+// let b = 2;
+ 
+// // console.log("a ** b =" , a ** b);
+// // console.log("a % b  =", a % b);
+// // console.log("a / b =", a / b);
+
+// // Unarry Operatorys like Incriment And Decriment 
+
+// console.log(a--
+
+// let a = 5;
+// let b = "5"; 
+// Arthemetic operator
+// a += 4 
+// a -= 5;
+// a *= 5; 
+// a /= 6;
+// a %= 7;
+// a **= 7;// Exponentation means the powers of value 
+
+// console.log("a =", a);// ans is  a + 4 +100 +104
+// console.log("a =" ,a);// ans is 100 - 4 = 95
+// console.log("a = ", a); // ans is 100 * 5 = 500
+// console.log("a =" , a); //modulo
+
+
+//  console.log("a = ", a); // 
+
+
+ // Comparison Operator 
+
+// console.log("a == b", a == b); // comparing 2 values like a  kya b ki taraha same hai 
+// console.log("a===b", a === b); //   strict version absalute equal 
+// console.log("a == b", a == b);
+// 
+// let a = 5;
+// let b = 6; 
+// console.log("a => b",a >= b);
+
+// Logical Operators
+
+// let a = 4;
+// let b = 4;
+
+
+// let cnd1 = a <= b;//true
+// let cnd2 = a === b;// true
+// console.log("cnd && cnd2",!(cnd1 <= cnd2));
+
+
+// Conditinal Statements///////////////////////
+
+// let age = 43;
+
+// if(age >= 18){
+//     console.log("You can not vote")
+// } else{
+//     console.log("You Rock")
+// };
+
+
+// Exe 1///////////////////////////////////////
+// let age = 18;
+
+//     if(age >= 18){
+//         console.log("You can Vote")
+//     };
+
+//     if(age < 18){
+//         console.log("You rock")
+//     };
+
+// Exe 2/////////////////////////////////////////
+
+// let mode ="dark";
+// let color;
+
+//     if(mode === "dark"){
+//         color = "black"
+//     };
+
+//     if(mode === "light"){
+//         color = "white"
+//     };
+
+//     console.log(color);
+
+// Exe 3///////////////////////////////////////////
+
+// let mode ="dark";
+// let color;
+
+// if(mode === "light"){
+//     color="black"
+// }else{
+//     color ="white"
+// };
+// console.log(color)
+
+
+// Exe 4/////////////////////////////////////////////
+
+
+// let age = 16;
+
+// if(age >= 18){
+//     console.log("Hello You Can Vote")
+// }else{
+//     console.log(" Hello Get Out from Here ")
+// };
+
+// Exe 5///////////////////////////////////////////////
+
+// let num = 117;
+
+// if(num%2 === 0){
+//     console.log(num,"is Even")
+// }else{
+//     console.log(num,"is Odd")
+// };
+
+// Exe 5////////////////////////////////////////////////
+
+// let mode ="dark";
+// let color;
+
+// if (mode ==="dark"){
+//     color = "Black"
+// }else if(mode === "blue"){
+//     color = "navyBlue"
+// }else if(mode === "yello"){
+//     color = "Dark yello"
+// }else{
+//     color = "Pink"
+// };
+// console.log(color);
+
+// if(mode === "dark")
+//     console.log(mode);
+
+
+// Exe 6/// Ternary Operatory/////////////////
+
+// let age = 32;
+// //  let result = age >=18 ? "You are adlult": "You are NOt An adult";
+
+//  age >=18 ? console.log("You are adlult"): console.log("You are NOt An adult");
+
+//  console.log(result)
+
+
+// Exe 7///////////////////////////////////////////////
+
+// Qs1. Get user to input a number using prompt("Enter a Number").Check a number is multiple of 5 or not.
+
+// Ans
+
+// let num = prompt("Enter a Number:");
+
+// if(num% 5 ===0){
+//     console.log(num,"Number is Modulo of 5")
+// }else{
+//     console.log(num,"Number is Not Modulo of 5")
+// }
+
+// Q2.Write a code which can give grade to students According to score.According.
+    // 90 to 100, A 
+    // 70 to 89, B 
+    // 50 to 69, C 
+    // 35 to 49, F
+// Ans.
+
+
+
