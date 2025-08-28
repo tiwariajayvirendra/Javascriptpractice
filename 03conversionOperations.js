16b574a65829899d49a999b1622997d9a2e95d9a
Tiwari Ajay Virendra <ajaytripathi821@gmail.com>
2025-08-28 17:15:22 +0530

    UPDated String Lactures

---
+++ 03conversionOperations.js
@@ -0,0 +1,28 @@
+let score = "HOP"
+
+//console.log(typeof score);
+//console.log(typeof(score));
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+//basics conversions
+
+let isLoggedIn = "Ja"
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+//1 => true; 0 => false
+// "" => false
+// "Hgi" => true
+
+let someNumber = 44
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
