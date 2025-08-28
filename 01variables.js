373d97d8ac5dafdb1b02a1b4868de3e48a85e724
Tiwari Ajay Virendra <ajaytripathi821@gmail.com>
2025-08-28 16:53:08 +0530

    Initial push of JavaScript practice code

---
+++ 01variables.js
@@ -0,0 +1,20 @@
+const accountId = 123123
+var accountEmail= "ajaytripathi@google.com"
+let  accountPassword = "2424"
+accountCity = "Gorakhpur"
+
+//accountId = 2 // not allowed this type of re declaration because it has already used const means constant no one can use this again after declaration
+accountEmail ="hc@a.com"
+accountPassword = "12123432"
+accountCity ="America"
+let accountState
+/* 
+Prefer not to use Var
+*/
+
+
+
+console.log(accountId);
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
+
---
+++ 02datatypes.js
@@ -0,0 +1,25 @@
+"use strict";// treat all js code a snewer version
+// alert("hello") // we are using nodejs, not browser
+
+console.log(
+    2-4)// code readability should be high 
+
+console.log("Ajay");
+
+let name =  "hitesh"
+let age = 28
+let isLoggedIn = false
+
+// Premitive Data type
+//number => 2 to power 53
+//bigint 
+//string => ""
+//boolean => true/false
+//null => standalone value hai it is a type 
+//undefined => 
+//symbol => when we say it is used for unique 
+
+//Object
+
+console.log(typeof undefined)// undefined
+console.log(typeof null)//object
